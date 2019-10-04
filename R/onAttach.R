d2e3cd81037739b2bed7c5d2c6c9ec878e2ff12a
Richard White <w@rwhite.no>
2019-10-04 12:54:58 +0300

    Update onAttach.R

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("PACKAGE: fhidata")
+  packageStartupMessage("PACKAGE: attrib")
   packageStartupMessage("Version: 2019.10.04 at 06:52")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
