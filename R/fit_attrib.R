ab03ef8e6fe701be90ba780a1d232daab1ebc439
Aurora <aurora-1@live.com>
2020-06-19 11:36:13 +0200

    improve test and fit

--- R/fit_attrib.R
+++ R/fit_attrib.R
@@ -8,18 +8,31 @@ fit_attrib <- function(
                        outcome = "deaths",
                        exposures =
                          list(
-                           "influenza" = "linear",
-                           "temperature" = "linear"
+                           "temperature_high" = "factor", #is it corect to call it this?
+                           "temperature_low" = "factor",
+                           "pr100_ili" = "linear",
+                           "is_winter" = "binary",
+                           "pop" = "offset"
                          )) {
   stopifnot(is.list(exposures))
   for (i in seq_along(exposures)) {
-    stopifnot(exposures[[i]] == "linear")
+    stopifnot(exposures[[i]] %in% c("linear", "factor", "binary", "offset"))
   }
 
   expo <- c()
   for (i in seq_along(exposures)) {
     if (exposures[[i]] == "linear") {
       expo <- c(expo, names(exposures)[i])
+    } else if (exposures[[i]] == "spline") {
+      temp <- glue::glue("splines::ns({names(exposures)[i]}, df=3)")
+      expo <- c(expo, temp)
+    } else if  (exposures[[i]] == "binary"){                         #is this correct?
+      expo <- c(expo, names(exposures)[i])
+    } else if  (exposures[[i]] == "factor"){                         #is this correct?
+      expo <- c(expo, names(exposures)[i])
+    } else if  (exposures[[i]] == "offset"){
+      offset <- glue::glue("offset({names(exposures)[i]})")
+      expo <- c(expo, offset)
     }
   }
   expo <- glue::glue_collapse(expo, sep = "+")
