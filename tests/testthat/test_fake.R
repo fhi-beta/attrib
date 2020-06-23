32fc10fa4b77eaa718b336aa2e7413984f100c11
Aurora <aurora-1@live.com>
2020-06-23 14:33:44 +0200

    update test to correct sin cos coefficiants

--- tests/testthat/test_fake.R
+++ tests/testthat/test_fake.R
@@ -53,7 +53,7 @@ test_that("Model fit", {
 
   testthat::expect_equal(
     round(as.numeric(results$temp, 0)),
-    c(-9, 7, 10, 0, 3, 0)                        #OBSOBS COEFICIANT DEPENDENT!!!!! #obsobs not sure if the oreder will always be the same when using the keyby..
+    c(-9, 0, 10, 0, 0, 0)                        #OBSOBS COEFICIANT DEPENDENT!!!!! #obsobs not sure if the oreder will always be the same when using the keyby..
   )
 })
 
@@ -61,27 +61,26 @@ test_that("Attributable numbers", {
 
   # generate data
   data <- gen_fake_attrib_data()
+  formula <- "deaths ~
+  (1|location_code) +
+  temperature_high +
+  pr100_ili_lag_1 +
+  (pr100_ili_lag_1|season) +
+  pr100_covid19_lag_1 +
+  sin(2 * pi * (week - 1) / 52) +
+  cos(2 * pi * (week - 1) / 52) +
+  offset(log(pop))"
 
   # fit initial model
   fit <- fit_attrib(
     data = data,
-    outcome = "deaths",
-    exposures = list(
-      "location_code" = "location",
-      "temperature_high" = "linear",
-      "temperature_low" = "linear",
-      "pr100_ili" = "linear",
-      "is_winter" = "linear",
-      "pr100_covid19" = "linear",
-      "pop" = "offset"
-    )
-  )
+    formula = formula)
 
   # create reference datasets
   data_observed <- copy(data)
 
-  data_reference_pr100_ili <- copy(data)
-  data_reference_pr100_ili[, pr100_ili := 0]
+  data_reference_pr100_ili_lag_1 <- copy(data)
+  data_reference_pr100_ili_lag_1[, pr100_ili_lag_1 := 0]
 
   data_reference_temperature_high <- copy(data)
   data_reference_temperature_high[, temperature_high := 0]
@@ -92,14 +91,14 @@ test_that("Attributable numbers", {
   data_reference_is_winter <- copy(data)
   data_reference_is_winter[, is_winter := 0]
 
-  data_reference_pr100_covid19 <- copy(data)
-  data_reference_pr100_covid19[, pr100_covid19 := 0]
+  data_reference_pr100_covid19_lag_1 <- copy(data)
+  data_reference_pr100_covid19_lag_1[, pr100_covid19_lag_1 := 0]
 
   # estimate attrib
-  est_pr100_ili <- est_attrib(
+  est_pr100_ili_lag_1 <- est_attrib(
     fit = fit,
     data_observed = data_observed,
-    data_reference = data_reference_pr100_ili
+    data_reference = data_reference_pr100_ili_lag_1
   )
 
   est_temperature_high <- est_attrib(
@@ -108,44 +107,32 @@ test_that("Attributable numbers", {
     data_reference = data_reference_temperature_high
   )
 
-  est_temperature_low <- est_attrib(
+
+  est_pr100_covid19_lag_1 <- est_attrib(
     fit = fit,
     data_observed = data_observed,
-    data_reference = data_reference_temperature_low
+    data_reference = data_reference_pr100_covid19_lag_1
   )
 
-  est_is_winter <- est_attrib(
-    fit = fit,
-    data_observed = data_observed,
-    data_reference = data_reference_is_winter
-  )
-
-  est_pr100_covid19 <- est_attrib(
-    fit = fit,
-    data_observed = data_observed,
-    data_reference = data_reference_pr100_covid19
-  )
-
-  data[, attrib_pr100_ili := est_pr100_ili]
+  data[, attrib_pr100_ili_lag_1 := est_pr100_ili_lag_1]
   data[, attrib_temperature_high := est_temperature_high]
   data[, attrib_temperature_low := est_temperature_low]
-  data[, attrib_is_winter := est_is_winter]
-  data[, attrib_pr100_covid19 := est_pr100_covid19]
+  data[, attrib_pr100_covid19_lag_1 := est_pr100_covid19_lag_1]
 
   # verify that your model is giving you results like you expect
   #influenza
-  testthat::expect_equal(sum(est_pr100_ili < 0), 0)
+  testthat::expect_equal(sum(est_pr100_ili_lag_1 < 0), 0)
 
   testthat::expect_lt(
-    sum(data[week >= 21 & week <= 39]$attrib_pr100_ili),
-    sum(data[week >= 40 | week <= 20]$attrib_pr100_ili)
+    sum(data[week >= 21 & week <= 39]$attrib_pr100_ili_lag_1),
+    sum(data[week >= 40 | week <= 20]$attrib_pr100_ili_lag_1)
   )
 
   #heat_wave
   testthat::expect_equal(sum(est_temperature_high < 0), 0)
 
   # is winter #må endres
-  testthat::expect_equal(sum(est_is_winter < 0), 0)
+  #testthat::expect_equal(sum(est_is_winter < 0), 0)
 
   # covid19
   testthat::expect_equal(sum(est_pr100_covid19 < 0), 0)
