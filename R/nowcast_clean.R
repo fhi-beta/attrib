e16141d04cc569540be3c90a48b8b03f60a8e5fd
Aurora <aurora-1@live.com>
2020-12-30 20:58:23 +0100

    skeleton nowcast_clean & fake_death data

---
+++ R/nowcast_clean.R
@@ -0,0 +1,34 @@
+
+
+
+
+
+#' For more details see the help vignette:
+#' \code{vignette("intro", package="attrib")}
+#'
+#' @param data Dataset containing DoE (Date of event) and DoR (Date of registation). The columns must have these exact names. 
+#' @param aggregation_date Date of aggregation 
+#' 
+nowcast_clean <- function(
+  data,
+  aggregation_date) {
+  
+  
+  # Do as in sykdomspulsen
+  # return only dataset or graphs as well? ## First only dataset! 
+  
+  # Create a fake dataset for testing :) 
+  
+  # Figure out something smart with NA
+  
+  # 
+  
+  
+  
+  retval <- 1
+  
+  
+  
+  
+  return (retval)
+}
