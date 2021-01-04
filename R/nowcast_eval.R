cf9b43eb9b50a37741e4050f6bbf039bc070866f
Aurora <aurora-1@live.com>
2021-01-04 14:14:15 +0100

    put in function

--- R/nowcast_eval.R
+++ R/nowcast_eval.R
@@ -1,3 +1,7 @@
-retval <- list()
-retval[1] <- list()<- # uke 1
-  retval[1]$plot_residular 
+
+nowcast <- function(){
+  retval <- list()
+  retval[1] <- list()<- # uke 1
+    retval[1]$plot_residular 
+}
+
